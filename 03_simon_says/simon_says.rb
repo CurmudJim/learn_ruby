0983137098bbce3153e8ed1a75ef0f2e522589ce
CurmudJim <jgarvey86@gmail.com>
2017-10-17 14:49:18 -0500

    One last thing to fix on 03

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,30 @@
 #write your code here
+def echo(a)
+  puts a
+  return a
+end
+
+def shout(a)
+  puts a
+  return a.upcase
+end
+
+def repeat(a , b = 1)
+  puts a
+  if b <= 1
+    b = 1
+  else
+    b -= 1
+  end
+  return a << (" " + a) * (b)
+end
+
+def start_of_word(a, b = 1)
+  puts a
+  return a[0,b]
+end
+
+def first_word(a)
+  puts a
+  return a[0,a.index(' ')]
+end
