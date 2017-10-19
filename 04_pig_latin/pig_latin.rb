632dfe43c5b1468e263d894133c866673ba88119
CurmudJim <jgarvey86@gmail.com>
2017-10-19 11:10:33 -0500

    Fixed 04 w/o bonus

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,15 @@
 #write your code here
+def translate(a)
+vowely = ['a','e','i','o','u','y']
+a = a.split
+print a
+a.each { |i|
+  until vowely.include?(i[0]) && (i[0] != 'u' && i[i.length-1] != 'q')
+    b = i[0]
+    i = i.reverse!.chop!.reverse! << b
+  end
+  i << "ay"
+}
+
+  a.join(" ")
+end
