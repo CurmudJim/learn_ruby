51f84e64b361792373132502e1495a6a16fee70e
CurmudJim <jgarvey86@gmail.com>
2017-10-31 16:09:47 -0500

    Fixed 05

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,12 @@
 class Book
-# write your code here
+	attr_accessor :title
+
+  def title
+    lowercase_words = ["a","an","the","for","and","nor","but","or","yet","so","at","around","by","after","along","for","from","in", "of","on","to","with","without"]
+    title = @title.split.each { |word| if !lowercase_words.include?(word); word.capitalize! end}
+		title.first.capitalize!
+    title.last.capitalize!
+    title.join ' '
+  end
+
 end
