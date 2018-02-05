c6b8c11aa134d0998766f8ea5c494097bf8401ad
CurmudJim <jgarvey86@gmail.com>
2018-02-05 12:52:23 -0600

    Did pig latin bonus

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,15 +1,21 @@
 #write your code here
-def translate(a)
+def translate(string)
 vowely = ['a','e','i','o','u','y']
-a = a.split
-print a
-a.each { |i|
-  until vowely.include?(i[0]) && (i[0] != 'u' && i[i.length-1] != 'q')
-    b = i[0]
-    i = i.reverse!.chop!.reverse! << b
+string = string.split
+string.each { |i|
+
+  until vowely.include?(i[0].downcase) && (i[0] != 'u' && i[i.length-1] != 'q')
+    firstLetter = i[0]
+    lastLetter = i[i.length-1]
+    i = i.reverse!.chop!.reverse!
+    if firstLetter.ord >= 65 && firstLetter.ord <= 90
+      firstLetter.downcase!
+      i.capitalize!
+    end
+     i <<  firstLetter
   end
   i << "ay"
 }
 
-  a.join(" ")
+  string.join(" ")
 end
